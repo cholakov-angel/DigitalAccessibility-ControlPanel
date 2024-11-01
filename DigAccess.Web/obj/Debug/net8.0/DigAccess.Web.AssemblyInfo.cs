da09ee61a844012ef97728a3824ef17efe4fb4d3
Angel Cholakov <145586496+cholakov-angel@users.noreply.github.com>
2024-11-01 20:36:15 +0200

    Fixing pages sizes.

--- DigAccess.Web/obj/Debug/net8.0/DigAccess.Web.AssemblyInfo.cs
+++ DigAccess.Web/obj/Debug/net8.0/DigAccess.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigAccess.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ccbb14bf40796070039c5f2d02663f2330a766")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ce7ba62f6acbb223e8f84151d34b26712c9fad")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigAccess.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigAccess.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
